42c7315acfe25d0ce78c24e85cd6a69bc8ff6ba7
Jeswanth Sirigiri <jeshusirigiri.2002@gmail.com>
2025-02-28 20:33:16 +0530

    Add files via upload

--- migrations/0001_initial.py
+++ migrations/0001_initial.py
@@ -1,14 +1,17 @@
+import django.contrib.admin.models
+from django.conf import settings
 from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("sites", "0001_initial"),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ("contenttypes", "__first__"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name="FlatPage",
+            name="LogEntry",
             fields=[
                 (
                     "id",
@@ -20,49 +23,53 @@ class Migration(migrations.Migration):
                     ),
                 ),
                 (
-                    "url",
-                    models.CharField(max_length=100, verbose_name="URL", db_index=True),
-                ),
-                ("title", models.CharField(max_length=200, verbose_name="title")),
-                ("content", models.TextField(verbose_name="content", blank=True)),
-                (
-                    "enable_comments",
-                    models.BooleanField(default=False, verbose_name="enable comments"),
+                    "action_time",
+                    models.DateTimeField(auto_now=True, verbose_name="action time"),
                 ),
                 (
-                    "template_name",
-                    models.CharField(
-                        help_text=(
-                            "Example: “flatpages/contact_page.html”. If this isn’t "
-                            "provided, the system will use “flatpages/default.html”."
-                        ),
-                        max_length=70,
-                        verbose_name="template name",
+                    "object_id",
+                    models.TextField(null=True, verbose_name="object id", blank=True),
+                ),
+                (
+                    "object_repr",
+                    models.CharField(max_length=200, verbose_name="object repr"),
+                ),
+                (
+                    "action_flag",
+                    models.PositiveSmallIntegerField(verbose_name="action flag"),
+                ),
+                (
+                    "change_message",
+                    models.TextField(verbose_name="change message", blank=True),
+                ),
+                (
+                    "content_type",
+                    models.ForeignKey(
+                        on_delete=models.SET_NULL,
                         blank=True,
+                        null=True,
+                        to="contenttypes.ContentType",
+                        verbose_name="content type",
                     ),
                 ),
                 (
-                    "registration_required",
-                    models.BooleanField(
-                        default=False,
-                        help_text=(
-                            "If this is checked, only logged-in users will be able to "
-                            "view the page."
-                        ),
-                        verbose_name="registration required",
+                    "user",
+                    models.ForeignKey(
+                        to=settings.AUTH_USER_MODEL,
+                        on_delete=models.CASCADE,
+                        verbose_name="user",
                     ),
-                ),
-                (
-                    "sites",
-                    models.ManyToManyField(to="sites.Site", verbose_name="sites"),
                 ),
             ],
             options={
-                "ordering": ["url"],
-                "db_table": "django_flatpage",
-                "verbose_name": "flat page",
-                "verbose_name_plural": "flat pages",
+                "ordering": ["-action_time"],
+                "db_table": "django_admin_log",
+                "verbose_name": "log entry",
+                "verbose_name_plural": "log entries",
             },
             bases=(models.Model,),
+            managers=[
+                ("objects", django.contrib.admin.models.LogEntryManager()),
+            ],
         ),
     ]
